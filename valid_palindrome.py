59990c639c3fc91f5f6bc999182753d7c0e36457
Yev Horokhovskykh <tech.dev.yev@gmail.com>
2024-02-25 22:14:40 -0500

    update

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -2,13 +2,13 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         text1 = s.lower()
         text2 = "".join(i for i in text1 if i.isalnum())
-        p1 = 0
-        p2 = -1
+        l = 0
+        r = -1
         res = True
         for i in text2:
-            if text2[p1] == text2[p2]:
-                p1 += 1
-                p2 -= 1
+            if text2[l] == text2[r]:
+                l += 1
+                r -= 1
             else:
                 res = False
 
